8d108b9cbd55288b98eebe81ead6989c721b48a0
Eric Nagy <eric@Mac-mini-de-Eric.local>
2026-04-11 14:49:19 -0700

    Fix type error

--- commands/apply-preset.ts
+++ commands/apply-preset.ts
@@ -98,7 +98,7 @@ async function applySettings({
         break;
       }
 
-      case "TVConfig": {
+      case "TvConfig": {
         await controlClient("SetTvConfig", {
           TvConfig: config,
         });
