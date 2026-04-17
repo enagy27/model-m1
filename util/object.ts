ecfddc044778d4649717df2a54d7f893f1e99bd6
Eric Nagy <eric@Mac-mini-de-Eric.local>
2026-04-16 22:31:22 -0700

    Add prettier. Include a pre-commit hook and CI checks

--- util/object.ts
+++ util/object.ts
@@ -1,21 +1,29 @@
 export type NonNullishValues<T> = {
-    [K in keyof T as T[K] extends null | undefined ? never : K]: T[K];
+  [K in keyof T as T[K] extends null | undefined ? never : K]: T[K];
 };
 
 type Entries<T> = { [K in keyof T]: [K, T[K]] }[keyof T][];
 
 export function entries<T extends Record<string, unknown>>(obj: T): Entries<T> {
-    return Object.entries(obj) as Entries<T>;
+  return Object.entries(obj) as Entries<T>;
 }
 
-export function fromEntries<K extends string, V>(entries: ReadonlyArray<readonly [K, V]>): Record<K, V> {
-    return Object.fromEntries(entries) as Record<K, V>;
+export function fromEntries<K extends string, V>(
+  entries: ReadonlyArray<readonly [K, V]>,
+): Record<K, V> {
+  return Object.fromEntries(entries) as Record<K, V>;
 }
 
-export function removeNullishValueEntries<T extends Record<string, unknown>>(obj: T): NonNullishValues<T> {
-    return Object.fromEntries(Object.entries(obj).filter(([, value]) => value != null)) as NonNullishValues<T>;
+export function removeNullishValueEntries<T extends Record<string, unknown>>(
+  obj: T,
+): NonNullishValues<T> {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([, value]) => value != null),
+  ) as NonNullishValues<T>;
 }
 
-export function isEmptyObject<T extends Record<string, unknown>>(obj: T): boolean {
-    return Object.entries(obj).length < 1;
+export function isEmptyObject<T extends Record<string, unknown>>(
+  obj: T,
+): boolean {
+  return Object.entries(obj).length < 1;
 }
