f789899b5904144d0f9273d520749cf7237f4073
Eric Nagy <eric@Mac-mini-de-Eric.local>
2026-04-20 22:26:22 -0700

    Add rule against relative imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import importX from "eslint-plugin-import-x";
 import perfectionist from "eslint-plugin-perfectionist";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
@@ -12,5 +13,25 @@ export default defineConfig(
   prettier,
   {
     ignores: ["dist/", "node_modules/"],
+  },
+  {
+    plugins: {
+      "import-x": importX,
+    },
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              // Enforce # absolute imports over ../ relative imports
+              group: ["../*"],
+              message:
+                "Use absolute imports (#util/example.js) instead of relative imports (../util/example.js)",
+            },
+          ],
+        },
+      ],
+    },
   },
 );
